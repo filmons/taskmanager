04f7056224c39d55de0c44d1ed1f086e2078f26f
filmonwmr <filmon.seare@windmyroof.com>
2024-04-08 12:08:26 +0200

    Firebase configuré pour les plateformes Android, iOS, macOS et Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()  async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // await Firebase.initializeApp();
+
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+
+// // ...
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
